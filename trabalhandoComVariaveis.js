d73fd85d8fe0c45d5a4fd9e8e499c34750591d32
Ana Carolina Vitorio <anacarolinavitorio@iMac-de-Ana.local>
2022-04-07 20:06:09 -0300

    Adicionando comentários no arquivo trabalhandoComVariaveis.

--- trabalhandoComVariaveis.js
+++ trabalhandoComVariaveis.js
@@ -11,11 +11,13 @@ const t2 = "2";
 console.log(t1+t2);
 
 //convertendo para inteiro
+//com o let é possível sobrescrever a variável reatribuindo um novo valor.
 let t1nNumero = parseInt(t1);
 let t2Numero = parseInt(t2);
 
 console.log(t1nNumero+t2Numero);
 
+// atribuindo um novo valor as variáveis.
 t1nNumero = 10;
 t2Numero = 20;
 console.log(t1nNumero+t2Numero);
