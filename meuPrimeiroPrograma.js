dbc41b55d9606c6bd18bc334e12bf59809b2cd43
Ana Carolina Vitorio <anacarolinavitorio@iMac-de-Ana.local>
2022-04-07 19:01:19 -0300

    Criando meu primeiro programa em java script.

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Hello word!");
+
+const meuNome = "Carol";
+var minhaIdade = 22;
+
+const nomeIrmao = "Lucas";
+var idadeIrmao = minhaIdade + 4;
+
+
+console.log(meuNome+" tem "+minhaIdade+" anos de idade e seu irmão "+nomeIrmao+" tem "+idadeIrmao+" anos de idade.");
+
+//sem utilizar o operador +
+console.log(`${meuNome} tem ${minhaIdade} anos de idade e seu irmão ${nomeIrmao} tem ${idadeIrmao} anos de idade.`);
