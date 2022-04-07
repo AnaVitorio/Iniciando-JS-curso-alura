7fe0c0abe33f0ed11e0ab2a4719eda885161bb5d
Ana Carolina Vitorio <anacarolinavitorio@iMac-de-Ana.local>
2022-04-07 20:03:56 -0300

    Trabalhando com variáveis, conversão de tipo e palavra let.

---
+++ trabalhandoComVariaveis.js
@@ -0,0 +1,25 @@
+const numero = 2022;
+const numeroRepresenta = "ano";
+
+console.log(numeroRepresenta+numero);
+console.log(numero+2000);
+console.log(typeof(numero));
+
+
+const t1 = "7";
+const t2 = "2";
+console.log(t1+t2);
+
+//convertendo para inteiro
+let t1nNumero = parseInt(t1);
+let t2Numero = parseInt(t2);
+
+console.log(t1nNumero+t2Numero);
+
+t1nNumero = 10;
+t2Numero = 20;
+console.log(t1nNumero+t2Numero);
+
+
+
+
