05e33d665a476071c5e4d222a65f22ca3ae32ee2
Ana Carolina Vitorio <anacarolinavitorio@iMac-de-Ana.local>
2022-04-07 21:44:37 -0300

    Trabalhando com loops while e for.

---
+++ loops.js
@@ -0,0 +1,25 @@
+console.log("Contando até 10:");
+let contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
+
+
+
+
+let numero = 1;
+while(numero <= 10){
+    if(numero % 2 == 0){
+        console.log(`${numero} é par!`);
+    } else{
+        console.log(`${numero} é ímpar`);
+    }
+    numero++
+}
+
+
+
+for(let i = 0; i<=10; i++){
+    console.log(i);         
+}
