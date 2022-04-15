1a879217b65ae6eabff6866cffc88e90337ef321
Ana Carolina Vitorio <anacarolinavitorio@iMac-de-Ana.local>
2022-04-15 17:58:38 -0300

    Adicionando get e set para atributos cliente e saldo.

--- orientacaoObjeto/ContaCorrente.js
+++ orientacaoObjeto/ContaCorrente.js
@@ -1,8 +1,24 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
-    numeroConta;
     agencia;
     _saldo = 0;
+    _cliente;
 
+
+    set cliente(cliente){
+        if(cliente instanceof Cliente){
+            this._cliente = cliente;
+        }
+        
+    }
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
 
 
     depositar(valor){
--- orientacaoObjeto/index.js
+++ orientacaoObjeto/index.js
@@ -9,9 +9,9 @@ cliente.cpf = 11122233345;
 cliente.email = "carol2@gmail.com";
 
 const contaCorrenteCarol = new ContaCorrente();
-contaCorrenteCarol.numeroConta =  12345;
 contaCorrenteCarol.agencia = 1001;
 contaCorrenteCarol.depositar(100);
+contaCorrenteCarol.cliente = cliente;
 
 const cliente2 = new Cliente();
 
@@ -20,11 +20,8 @@ cliente2.cpf = 22222222234;
 cliente2.email = "lucas@gmail.com";
 
 const contaCorrenteLucas = new ContaCorrente();
-
-contaCorrenteLucas.numeroConta =  197536;
 contaCorrenteLucas.agencia = 1002;
+contaCorrenteLucas.cliente = cliente2;
 
-console.log(contaCorrenteCarol);
+
 contaCorrenteCarol.transferir(100, contaCorrenteLucas);
-console.log(contaCorrenteCarol);
-console.log(contaCorrenteLucas);
